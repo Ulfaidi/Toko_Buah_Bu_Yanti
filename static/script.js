db9018f1fbaf1643a0a16c5c8be4a90bbe734968
ulfaidi <ulfaidi2002@gmail.com>
2024-06-16 12:47:50 +0700

    add

--- static/script.js
+++ static/script.js
@@ -66,3 +66,26 @@ function previewFile(file) {
     content.style.display = "none";
   };
 }
+
+// inputan pencarian
+function searchTable() {
+  var input, filter, table, tr, td, i, j, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toLowerCase();
+  table = document.getElementById("dataTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 1; i < tr.length; i++) {
+    tr[i].style.display = "none"; 
+    td = tr[i].getElementsByTagName("td");
+    for (j = 0; j < td.length; j++) {
+      if (td[j]) {
+        txtValue = td[j].textContent || td[j].innerText;
+        if (txtValue.toLowerCase().indexOf(filter) > -1) {
+          tr[i].style.display = ""; 
+          break;
+        }
+      }
+    }
+  }
+}
